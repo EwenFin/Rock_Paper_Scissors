918d6af2e850cfa47af6dbb31c20f63571a32c25
Ewen Carr <ewencarr@hotmail.com>
2017-02-20 18:01:14 +0000

    added controller, returning result as a string

---
+++ controller.rb
@@ -0,0 +1,11 @@
+require ('sinatra')
+require ('sinatra/contrib/all')
+require ('pry')
+require ('./models/rock_paper_scissors.rb')
+
+get '/play/:hand1/:hand2' do
+  game = Game.new(params[:hand1], params[:hand2])
+  @victor = game.play
+  erb(:result)
+  # return "#{game.play}"
+end
